cf6618f05088160227fe6a67a2ee6717525cd397
Iuiiss <89552100+Iuiiss@users.noreply.github.com>
2021-08-25 17:57:57 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+Hello
+*
+Luis Leis Campos
+8/25/21
+CMSC255 CRN 41688
+*/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Luis Leis Campos");
+		System.out.println("Hello");
+		System.out.println("Hello/Lab1");
+		System.out.println("8/25/21");
+		System.out.println("CMSC255 and CRN 41688");
+		System.out.println();
+	}
+}
